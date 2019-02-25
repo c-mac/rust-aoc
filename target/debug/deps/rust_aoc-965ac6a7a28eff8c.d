7c3045c0d0cce4406da72485e9e075230ad6cc43
Cameron McCallie <cam@nomadhealth.com>
2019-02-25 17:00:32 -0500

    cargo stuff

--- target/debug/deps/rust_aoc-965ac6a7a28eff8c.d
+++
@@ -1,5 +0,0 @@
-/Users/cam/Code/rust-aoc/target/debug/deps/rust_aoc-965ac6a7a28eff8c: src/main.rs
-
-/Users/cam/Code/rust-aoc/target/debug/deps/rust_aoc-965ac6a7a28eff8c.d: src/main.rs
-
-src/main.rs:
--- target/debug/rust-aoc.d
+++
@@ -1,1 +0,0 @@
-/Users/cam/Code/rust-aoc/target/debug/rust-aoc.dSYM: /Users/cam/Code/rust-aoc/src/main.rs
